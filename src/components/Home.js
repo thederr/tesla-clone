4dcd2beeb3c13fddd4d284c77715d2c63a6076cd
derrick matheny <mathenyderrick@gmail.com>
2021-05-30 23:50:03 -0400

    utilized destruc to auto populate section component based upon props passed to comp

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,45 @@
-import React from 'react';
-import styled from 'styled-components';
-import Section from './Section';
+import React from "react";
+import styled from "styled-components";
+import Section from "./Section";
 
 function Home() {
-    return (
-        <Container>
-            <Section/>
-        </Container>
-    )
+  return (
+    <Container>
+      <Section
+        title='Model S'
+        description='Order Online for Touchless Delivery'
+        backgroundImg='model-s.jpg'
+        leftBtnText='Custom Order'
+        RightBtnText='Existing Inventory'
+      />
+      <Section
+        title='Model Y'
+        description='Order Online for Touchless Delivery'
+        backgroundImg='model-y.jpg'
+        leftBtnText='Custom Order'
+        RightBtnText='Existing Inventory'
+      />
+      <Section
+        title='Model 3'
+        description='Order Online for Touchless Delivery'
+        backgroundImg='model-3.jpg'
+        leftBtnText='Custom Order'
+        RightBtnText='Existing Inventory'
+      />
+          <Section
+        title='Model X'
+        description='Order Online for Touchless Delivery'
+        backgroundImg='model-x.jpg'
+        leftBtnText='Custom Order'
+        RightBtnText='Existing Inventory'
+      />
+     
+    </Container>
+  );
 }
 
-export default Home
+export default Home;
 
 const Container = styled.div`
-    height: 100vh  ;
-
-`
+  height: 100vh;
+`;
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,19 +1,19 @@
 import React from "react";
 import styled from "styled-components";
 
-function Section() {
+function Section({title,description,backgroundImg,leftBtnText,RightBtnText}) {
   return (
     <>
-      <Wrap>
+      <Wrap bgImage={backgroundImg}>
         <ItemText>
-          <h1>Model S</h1>
-          <p>Order Online for Touchless Delivery</p>
+          <h1>{title}</h1>
+          <p>description</p>
         </ItemText>
         <Buttons>
           <ButtonGroup>
-            <LeftButton>Custom Order</LeftButton>
+            <LeftButton>{leftBtnText}</LeftButton>
 
-            <RightButton>Existing Inventory</RightButton>
+            <RightButton>{RightBtnText}</RightButton>
           </ButtonGroup>
           <DownArrow className src='/images/down-arrow.svg' />
         </Buttons>
@@ -30,8 +30,11 @@ const Wrap = styled.div`
   background-size: cover;
   background-position: center;
   background-repeat: not-repeat;
-  background-image: url("/images/model-s.jpg");
 
+//------------ Fancy happenings right here---------
+  background-image: ${props=>`url("/images/${props.bgImage}")`};
+//------------ Jesus this is fancy---------------
+  
   display: flex;
   flex-direction: column;
   justify-content: space-between; //vertical alignment
@@ -45,13 +48,17 @@ const ItemText = styled.div`
 const ButtonGroup = styled.div`
   display: flex;
   margin-bottom: 30px;
+  @media (max-width:768px){
+    flex-direction:column;
+  }
 `;
 const LeftButton = styled.div`
     background-color: rgba(23,26,32,0.8);
     height:40px;
     width:256px;
     color:white;
-    display-flex;
+    display:flex;
+
     justify-content:center;
     align-items: center;
     border-radius: 100px;
@@ -60,8 +67,6 @@ const LeftButton = styled.div`
     font-size:12px;
     cursor:pointer;
     margin:10px;
-    text-align-center;
-    justify-text:center;
 
     `;
 const RightButton = styled(LeftButton)`
