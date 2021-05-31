8243da61bc04683a5e55acca0388e543ead31e00
derrick matheny <mathenyderrick@gmail.com>
2021-05-31 01:30:04 -0400

    added close button threw it to the right side of the menu getting ready to use usestate to open and close this mf

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { Menu as MenuIcon } from "@material-ui/icons";
+import CloseIcon from "@material-ui/icons/Close";
 
 
 function Header() {
@@ -20,6 +21,17 @@ function Header() {
         <a href='#'>Tesla Account</a>
         <CustomMenu/>
       </RightMenu>
+      <BurgerNav>
+          <CloseWrapper>
+             <CustomClose/>
+          </CloseWrapper>
+      <li><a href="#">Existing Inventory</a></li>
+      <li><a href="#">Used Inventory</a></li>
+      <li><a href="#">Trade-In</a></li>
+      <li><a href="#">Cybertruck</a></li>
+      <li><a href="#">Roadster</a></li>
+      <li><a href="#">Existing Inventory</a></li>
+      </BurgerNav>
     </Container>
   );
 }
@@ -36,6 +48,7 @@ const Container = styled.div`
   top: 0;
   left: 0;
   right: 0; // expaning header the full width
+  z-index:1;
 `;
 
 const Menu = styled.div`
@@ -69,4 +82,38 @@ a {
 
 const CustomMenu = styled(MenuIcon)`
   cursor:pointer;
+`;
+
+const BurgerNav = styled.div`
+  position:fixed;
+  top:0;
+  bottom:0;
+  right:0;
+  background:white;
+  width:300px;
+  z-index:5;
+  list-style:none;
+  padding:20px;
+  display:flex;
+  flex-direction:column;
+  text-align:start;
+
+  li {
+     padding 15px 0;
+     border-bottom: 1x solid rgba(0,0,0,0.2);
+    
+     a{
+         font-weight:600;
+     }
+
+  }
+
+`;
+
+const CustomClose = styled(CloseIcon)`
+  cursor:pointer;
+`;
+const CloseWrapper = styled.div`
+  display:flex;
+  justify-content:flex-end;
 `;
